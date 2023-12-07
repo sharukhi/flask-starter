d6dd3203be73f1a3dbf434edd55c5d450c68b8d2
sharukhi <sharukhi@sharukhi.xyz>
2023-12-07 17:20:08 +0600

    Initial Commit

---
+++ api.py
@@ -0,0 +1,14 @@
+# Example api request 
+import requests
+
+# The API endpoint
+url = "api.url"
+
+# A GET request to the API
+response = requests.get(url)
+
+# Print the response
+response_json = response.json()
+
+print(response_json)
+
