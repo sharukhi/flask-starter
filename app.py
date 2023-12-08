49214a7eccf41b92c6c65315cc312c1c577a1811
sharukhi <sharukhi@sharukhi.xyz>
2023-12-08 12:39:49 +0600

    Initial Commit

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import *
 import requests
 import json 
+from init import *
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    init()
     return render_template('index.html')
 
 if __name__ == '__main__':
